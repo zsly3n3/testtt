3eef67c21b872520272fad511282f45005c0733f
wmj <zsly3n3@163.com>
2021-11-23 11:11:56 +0800

    add

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/p3", func(c *gin.Context) {
+		ipStr,err:=getLocalIP()
+        if err!=nil{
+			c.JSON(200, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": ipStr,
 		})
 	})
 	addr := fmt.Sprintf(`:%d`, 8180)
@@ -18,4 +27,30 @@ func main() {
 	if err!=nil{
 		log.Fatal(`run_err:`,err.Error())
 	}
+}
+
+func getLocalIP() (ipv4 string, err error) {
+	var (
+		addrs   []net.Addr
+		addr    net.Addr
+		ipNet   *net.IPNet // IP地址
+		isIpNet bool
+	)
+	// 获取所有网卡
+	if addrs, err = net.InterfaceAddrs(); err != nil {
+		return
+	}
+	// 取第一个非lo的网卡IP
+	for _, addr = range addrs {
+		// 这个网络地址是IP地址: ipv4, ipv6
+		if ipNet, isIpNet = addr.(*net.IPNet); isIpNet && !ipNet.IP.IsLoopback() {
+			// 跳过IPV6
+			if ipNet.IP.To4() != nil {
+				ipv4 = ipNet.IP.String() // 192.168.1.1
+				return
+			}
+		}
+	}
+	err = errors.New(`not found`)
+	return
 }
